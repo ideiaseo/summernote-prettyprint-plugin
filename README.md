86c39b0f621cfad121651bdaf577ba1843b7e6a4
Anniel Menezes <annielmenezes@ideiaseo.com>
2016-08-12 13:05:23 -0300

    #1 - Add summernote-prettyprint-plugin.js and example folder

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# summernote-plugin-prettyprint
+# Summernote Prettyprint Plugin
+
 Plugin that add prettyprinted code into editor
+
+## Installation
+
+```html
+<!-- include jquery -->
+<script   src="http://code.jquery.com/jquery-2.2.4.min.js"></script>
+
+<!-- include Bootstrap and Prettyprint -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prettify/r298/prettify.min.css" />
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/prettify/r298/prettify.min.js"></script>
+
+<!-- include summernote css/js -->
+<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.1/summernote.css" />
+<script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.1/summernote.js"></script>
+
+<!-- include summernote plugin -->
+<script src="summernote-prettyprint-plugin.js"></script>
+```
+
+## Usage
+
+Add `div` into `body`; this targeted element will later be rendered to summernote editing tool.
+
+```html
+<div id="summernote"></div>
+```
+Run the script below when document is ready!
+
+```html
+<script>
+  $(document).ready(function() {
+    $('#summernote').summernote();
+  });
+</script>
+```
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## History
+
+TODO: Write history
+
+## Credits
+
+TODO: Write credits
+
+## License
+
+[License](LICENSE)
---
+++ summernote-prettyprint-plugin.js
@@ -0,0 +1,208 @@
+(function(factory) {
+    /* global define */
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = factory(require('jquery'));
+    } else {
+        // Browser globals
+        factory(window.jQuery);
+    }
+}(function($) {
+  'use strict';
+
+  var summernotePrettyprint = function (context) {
+    var self = this;
+    var options = context.options;
+    var inToolbar = false;
+
+    for (var idx in options.toolbar) {
+      var buttons = options.toolbar[idx][1];
+      if ($.inArray('prettyprint', buttons) > -1) {
+        inToolbar = true;
+        break;
+      }
+    }
+
+    if (!inToolbar) { return; }
+
+    var ui = $.summernote.ui;
+    var $editor = context.layoutInfo.editor;
+    var lang = options.langInfo;
+
+    var prettyprintOpitions = {
+
+    };
+
+    options.prettyprint = $.extend(prettyprintOpitions, options.prettyprint);
+
+    context.memo('button.prettyprint', function () {
+      var button = ui.button({
+        contents: '{}',
+        tooltip: lang.button.tooltip,
+        click: function (event) {
+          self.show();
+        }
+      });
+
+      return button.render();
+    });
+
+    this.show = function () {
+      context.invoke('editor.saveRange');
+
+      self.showDialog()
+        .then(function showDialogCb (data) {
+          context.invoke('editor.restoreRange');
+          // Insert code prettyprinted into editor
+          // [function here]
+          self.insertToEditor(data);
+          ui.hideDialog(self.$dialog);
+        }).fail(function () {
+          context.invoke('editor.restoreRange');
+        });
+    };
+
+    this.showDialog = function () {
+      self.disableAddButton();
+      self.$code.value = '';
+
+      return $.Deferred(function (deferred) {
+        ui.onDialogShown(self.$dialog, function dialogShownCb () {
+          context.triggerEvent('dialog.shown');
+          self.$codeLanguage.focus();
+
+          self.$addBtn.on('click', function addEmbedCb (event) {
+            event.preventDefault();
+            deferred.resolve({
+              language: self.$codeLanguage.value,
+              code: self.$code.value
+            });
+          });
+        });
+
+        ui.onDialogHidden(self.$dialog, function dialogHiddenCb () {
+          self.$addBtn.off('click');
+          if (deferred.state() === 'pending') {
+            deferred.reject();
+          }
+        });
+
+        ui.showDialog(self.$dialog);
+      });
+    };
+
+    this.createDialog = function ($container) {
+
+      var languages = [
+          'bsh', 'c', 'cc', 'cpp', 'cs', 'csh', 'cyc', 'cv', 'htm', 'html',
+          'java', 'js', 'm', 'mxml', 'perl', 'pl', 'pm', 'py', 'php', 'rb',
+          'sh', 'xhtml', 'xml', 'xsl'
+      ];
+
+      var select = '<select class="form-control" id="code-language"><option>' + languages.join('</option><option>') + '</option></select>';
+
+      var dialogOption = {
+        title: lang.dialog.title,
+        body: '<div class="form-group">' +
+        '<label>' + lang.dialog.selectLabel + '</label>' +
+        select +
+        '</div>' +
+        '<div class="form-group">' +
+        '<label>' + lang.dialog.codeLabel + '</label>' +
+        '<textarea class="form-control" id="code" rows="7"></textarea>' +
+        '</div>',
+        footer: '<button href="#" id="btn-add" class="btn btn-primary">' + lang.dialog.button + '</button>',
+        closeOnEscape: true
+      };
+
+      self.$dialog = ui.dialog(dialogOption).render().appendTo($container);
+      self.$addBtn = self.$dialog.find('#btn-add');
+      self.$codeLanguage = self.$dialog.find('#code-language')[0];
+      self.$code = self.$dialog.find('#code')[0];
+    };
+
+    this.insertToEditor = function (options) {
+      var innerOptions = {
+        code: '',
+        language: ''
+      };
+
+      var _options = $.extend(innerOptions, options);
+
+      var $node = $('<pre>', {
+        class: 'prettyprint linenums:0'
+      });
+      var $code = $('<code>');
+      $code.html(_options.code.replace(/</g,"&lt;").replace(/>/g,"&gt;"));
+      $code.addClass('language-' + _options.language);
+
+      $node.html($code)
+
+      context.invoke('editor.insertNode', $node[0]);
+      self.$code.innerHTML = '';
+    };
+
+    this.enableAddButton = function() {
+      if (!!self.$codeLanguage.value && self.$code.value.length > 0) {
+        self.$addBtn.attr("disabled", false);
+      }
+    };
+
+    this.disableAddButton = function() {
+      self.$addBtn.attr("disabled", true);
+    };
+
+    this.init = function () {
+        self.$code.addEventListener('input', function (event) {
+          self.enableAddButton();
+        }, false);
+        self.$codeLanguage.addEventListener('change', function (event) {
+          self.enableAddButton();
+        }, false);
+    };
+
+    this.initialize = function() {
+      var $container = options.dialogsInBody ? $(document.body) : $editor;
+      self.createDialog($container);
+    };
+
+    this.destroy = function() {
+      ui.hideDialog(self.$dialog);
+      self.$dialog.remove();
+    };
+
+    this.events = {
+      'summernote.init': function(we, e) {
+
+        self.init();
+      },
+      'summernote.change': function (we, contents) {
+        prettyPrint();
+        console.log(contents);
+      }
+    };
+  };
+
+  $.extend(true, $.summernote, {
+    lang: {
+      'en-US': {
+        dialog: {
+          title: 'Insert fragment code into editor',
+          selectLabel: 'Choose your language',
+          codeLabel: 'Type your code',
+          button: 'Insert code'
+        },
+        button: {
+          tooltip: 'Insert code'
+        }
+      },
+      'pt-BR': {}
+    },
+    plugins: {
+      'prettyprint': summernotePrettyprint
+    }
+  });
+}));
